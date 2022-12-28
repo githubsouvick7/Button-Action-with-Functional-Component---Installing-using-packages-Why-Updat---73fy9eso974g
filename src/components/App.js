f5416b915ec565160dc1f627683769aac6cf1c45
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2022-12-28 22:22:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+const [readMore,setReadMore] = useState(false);
+  const extraContent=<div>
+      <p id="para">
+      Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
+  </div>
   return (
-    <div id="main">
-      // Do not alter the main div
+    <div className="App">
+      <button id="click" onClick={()=>{setReadMore(!readMore)}}>Click</button>
+      {readMore && extraContent}
     </div>
   );
 }
