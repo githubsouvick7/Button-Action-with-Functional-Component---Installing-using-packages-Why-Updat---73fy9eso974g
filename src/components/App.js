a69656873387a45cf8d76862cb7baf42f4688c26
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-01-08 23:19:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, { useState } from 'react';
 
 function App() {
-const [readMore,setReadMore] = useState(false);
-  const extraContent=<div>
-      <p id="para">
-      Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-      </p>
-  </div>
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const handleClick = () => {
+    setShowParagraph(true);
+  }
+
   return (
-    <div className="App">
-      <button id="click" onClick={()=>{setReadMore(!readMore)}}>Click</button>
-      {readMore && extraContent}
+    <div>
+      <button id="click" onClick={handleClick}>Click me</button>
+      {showParagraph && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
-
 
 export default App;
